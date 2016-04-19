64daa1e82883b43296adc57a318b9280a0449458
Ryan Rayfield <ryan.rayfield@ussquash.com>
2016-04-19 13:37:13 -0400

    update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ var mozjpeg = require('imagemin-mozjpeg');
 
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
     grunt.initConfig({
         imagemin: {                          // Task
             static: {                          // Target
@@ -15,10 +15,10 @@ module.exports = function(grunt) {
                 files: {                         // Dictionary of files
                     'frontend-nanodegree-mobile-portfolio/dist/views/images/pizzeria.jpg': 'frontend-nanodegree-mobile-portfolio/src/views/images/pizzeria.jpg', // 'destination': 'source'
                     'frontend-nanodegree-mobile-portfolio/dist/views/images/pizza.png': 'frontend-nanodegree-mobile-portfolio/src/views/images/pizza.png', // 'destination': 'source'
-                    'frontend-nanodegree-mobile-portfolio/src/img/2048.png': 'frontend-nanodegree-mobile-portfolio/dist/img/2048.png', // 'destination': 'source'
-                    'frontend-nanodegree-mobile-portfolio/src/img/cam_be_like.jpg': 'frontend-nanodegree-mobile-portfolio/dist/img/cam_be_like.jpg', // 'destination': 'source'
-                    'frontend-nanodegree-mobile-portfolio/src/img/mobilewebdev.png': 'frontend-nanodegree-mobile-portfolio/dist/img/mobilewebdev.png', // 'destination': 'source'
-                    'frontend-nanodegree-mobile-portfolio/src/img/profilepic.jpg': 'frontend-nanodegree-mobile-portfolio/dist/img/profilepic.jpg', // 'destination': 'source'
+                    'frontend-nanodegree-mobile-portfolio/dist/img/2048.png': 'frontend-nanodegree-mobile-portfolio/src/img/2048.png', // 'destination': 'source'
+                    'frontend-nanodegree-mobile-portfolio/dist/img/cam_be_like.jpg': 'frontend-nanodegree-mobile-portfolio/src/img/cam_be_like.jpg', // 'destination': 'source'
+                    'frontend-nanodegree-mobile-portfolio/dist/img/mobilewebdev.png': 'frontend-nanodegree-mobile-portfolio/src/img/mobilewebdev.png', // 'destination': 'source'
+                    'frontend-nanodegree-mobile-portfolio/dist/img/profilepic.jpg': 'frontend-nanodegree-mobile-portfolio/src/img/profilepic.jpg', // 'destination': 'source'
                                     }
                  },
             //     dynamic: {                         // Another target
@@ -33,6 +33,6 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-critical');
     grunt.registerTask('default', ['imagemin']);
+
 }
